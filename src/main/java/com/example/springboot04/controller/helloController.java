9804f09829f06e01abf7f5a239e3dcf909faaeef
littlehubotao <https://github.com/littlehubotao/>
2020-03-15 21:20:37 +0800

    Initial commit

---
+++ src/main/java/com/example/springboot04/controller/helloController.java
@@ -0,0 +1,25 @@
+package com.example.springboot04.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class helloController {
+
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return  "11111";
+    }
+
+
+
+    @RequestMapping("/hello2")
+    public String hello2(){
+        return  "hello";
+    }
+
+}
